307ff0215b0eab606067a8b821ff97aeb257e2f6
Luisa-bee <67885313+Luisa-bee@users.noreply.github.com>
2020-07-29 16:55:37 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Hi im Luisa and this is my first atempt to do the Peer-graded 
+## Assignment: Programming Assignment 2: Lexical Scoping
 
-## Write a short comment describing this function
+## I would love if you guys can say something about the way I indented my code, 
+## and if the coments I made are good, because I think I strugle the most with that.
+## thanks a lot :) 
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse
 
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+    s <- NULL
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) s <<- solve
+    getsolve <- function() s
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix 
+## above. If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    s <- x$getsolve()
+    if(!is.null(s)) {
+        message("getting inversed matrix")
+        return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setsolve(s)
+    s
+} 
